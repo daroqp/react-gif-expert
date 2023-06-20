bcd7172a91e65cd0721cc9e34932060359a3bc4f
Dario Quispe <daro.qp@gmail.com>
2023-06-19 21:43:31 -0300

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newInputValue = inputValue.trim();
+    if (inputValue.length <= 1) return;
+
+    onNewCategory(newInputValue);
+
+    setInputValue("");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
